140eb31be1ee35ff85b90dac5857cb4edc1719db
smartiful-rui-qi <a6504130@gmail.com>
2022-02-22 00:20:11 +0900

    update post layout

--- layouts/post-layout.jsx
+++ layouts/post-layout.jsx
@@ -2,15 +2,30 @@ import Link from '@/components/link'
 import PageTitle from '@/components/page-title'
 import SectionContainer from '@/components/section-container'
 import { BlogSeo } from '@/components/seo'
-import Image from 'next/image'
+import Image from '@/components/image'
 import Tag from '@/components/tag'
 import siteMetadata from '@/data/site-metadata'
 import useTranslation from 'next-translate/useTranslation'
 import formatDate from '@/lib/utils/formatDate'
 import { useRouter } from 'next/router'
 
-export default function PostLayout({ frontMatter, authorDetails, next, prev, children }) {
-  const { slug, date, title, tags } = frontMatter
+const editUrl = (fileName) => `${siteMetadata.siteRepo}/blob/master/data/blog/${fileName}`
+const discussUrl = (slug) =>
+  `https://mobile.twitter.com/search?q=${encodeURIComponent(
+    `${siteMetadata.siteUrl}/blog/${slug}`
+  )}`
+
+const postDateTemplate = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }
+
+export default function PostLayout({
+  frontMatter,
+  authorDetails,
+  next,
+  prev,
+  availableLocales,
+  children,
+}) {
+  const { slug, fileName, date, title, tags } = frontMatter
   const { t } = useTranslation()
   const { locale } = useRouter()
 
@@ -19,33 +34,32 @@ export default function PostLayout({ frontMatter, authorDetails, next, prev, chi
       <BlogSeo
         url={`${siteMetadata.siteUrl}/blog/${slug}`}
         authorDetails={authorDetails}
+        availableLocales={availableLocales}
         {...frontMatter}
       />
-      <article className="flex justify-center w-full mt-5">
+      <article>
         <div className="xl:divide-y xl:divide-gray-200 xl:dark:divide-gray-700">
           <header className="pt-6 xl:pb-6">
             <div className="space-y-1 text-center">
-              <PageTitle>{title}</PageTitle>
+              <dl className="space-y-10">
+                <div>
+                  <dt className="sr-only">{t('common:pub')}</dt>
+                  <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
+                    <time dateTime={date}>{formatDate(new Date(date), locale)}</time>
+                  </dd>
+                </div>
+              </dl>
+              <div>
+                <PageTitle>{title}</PageTitle>
+              </div>
             </div>
           </header>
-          {/* tags & 作者栏 */}
-          <div className="flex flex-row items-center content-center justify-center space-x-5">
-            <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
-              <time dateTime={date}>{formatDate(new Date(date), locale)}</time>
-            </dd>
-            {tags && (
-              <div className="py-4 xl:py-8">
-                <h2 className="text-xs tracking-wide text-gray-500 uppercase dark:text-gray-400">
-                  Tags
-                </h2>
-                <div className="flex flex-wrap">
-                  {tags.map((tag) => (
-                    <Tag key={tag} text={tag} />
-                  ))}
-                </div>
-              </div>
-            )}
-            <div>
+          <div
+            className="pb-8 divide-y divide-gray-200 xl:divide-y-0 dark:divide-gray-700 xl:grid xl:grid-cols-4 xl:gap-x-6"
+            style={{ gridTemplateRows: 'auto 1fr' }}
+          >
+            <dl className="pt-6 pb-10 xl:pt-11 xl:border-b xl:border-gray-200 xl:dark:border-gray-700">
+              <dt className="sr-only">{t('common:authors')}</dt>
               <dd>
                 <ul className="flex justify-center space-x-8 xl:block sm:space-x-12 xl:space-x-0 xl:space-y-8">
                   {authorDetails.map((author) => (
@@ -78,50 +92,65 @@ export default function PostLayout({ frontMatter, authorDetails, next, prev, chi
                   ))}
                 </ul>
               </dd>
-            </div>
-          </div>
-          {/* blog */}
-          <div className="pb-8 divide-y divide-gray-200">
+            </dl>
             <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:pb-0 xl:col-span-3 xl:row-span-2">
-              <div className="flex flex-col content-center justify-center w-full pt-10 pb-8 prose dark:prose-dark">
-                {children}
+              <div className="pt-10 pb-8 prose dark:prose-dark max-w-none">{children}</div>
+              <div className="pt-6 pb-6 text-sm text-gray-700 dark:text-gray-300">
+                <Link href={discussUrl(slug)} rel="nofollow">
+                  {t('common:twitter')}
+                </Link>
+                {` • `}
+                <Link href={editUrl(fileName)}>{t('common:github')}</Link>
               </div>
             </div>
-            {/* footer */}
-            <div className="text-sm font-medium leading-5 divide-gray-200 xl:divide-y dark:divide-gray-700 xl:col-start-1 xl:row-start-2">
-              {(next || prev) && (
-                <div className="flex justify-between py-4 xl:block xl:space-y-8 xl:py-8">
-                  {prev && (
-                    <div>
-                      <h2 className="text-xs tracking-wide text-gray-500 uppercase dark:text-gray-400">
-                        {t('common:preva')}
-                      </h2>
-                      <div className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">
-                        <Link href={`/blog/${prev.slug}`}>{prev.title}</Link>
-                      </div>
+            <footer>
+              <div className="text-sm font-medium leading-5 divide-gray-200 xl:divide-y dark:divide-gray-700 xl:col-start-1 xl:row-start-2">
+                {tags && (
+                  <div className="py-4 xl:py-8">
+                    <h2 className="text-xs tracking-wide text-gray-500 uppercase dark:text-gray-400">
+                      Tags
+                    </h2>
+                    <div className="flex flex-wrap">
+                      {tags.map((tag) => (
+                        <Tag key={tag} text={tag} />
+                      ))}
                     </div>
-                  )}
-                  {next && (
-                    <div>
-                      <h2 className="text-xs tracking-wide text-gray-500 uppercase dark:text-gray-400">
-                        {t('common:nexta')}
-                      </h2>
-                      <div className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">
-                        <Link href={`/blog/${next.slug}`}>{next.title}</Link>
+                  </div>
+                )}
+                {(next || prev) && (
+                  <div className="flex justify-between py-4 xl:block xl:space-y-8 xl:py-8">
+                    {prev && (
+                      <div>
+                        <h2 className="text-xs tracking-wide text-gray-500 uppercase dark:text-gray-400">
+                          {t('common:preva')}
+                        </h2>
+                        <div className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">
+                          <Link href={`/blog/${prev.slug}`}>{prev.title}</Link>
+                        </div>
                       </div>
-                    </div>
-                  )}
-                </div>
-              )}
-            </div>
-            <div className="pt-4 xl:pt-8">
-              <Link
-                href="/blog"
-                className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-              >
-                &larr; {t('common:back')}
-              </Link>
-            </div>
+                    )}
+                    {next && (
+                      <div>
+                        <h2 className="text-xs tracking-wide text-gray-500 uppercase dark:text-gray-400">
+                          {t('common:nexta')}
+                        </h2>
+                        <div className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">
+                          <Link href={`/blog/${next.slug}`}>{next.title}</Link>
+                        </div>
+                      </div>
+                    )}
+                  </div>
+                )}
+              </div>
+              <div className="pt-4 xl:pt-8">
+                <Link
+                  href="/blog"
+                  className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+                >
+                  &larr; {t('common:back')}
+                </Link>
+              </div>
+            </footer>
           </div>
         </div>
       </article>
