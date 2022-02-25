9026ee49c34b052beda406e61c969dfce0e6ab2a
RUI <>
2022-02-26 06:45:23 +0900

    update footer, header dark mode style

--- components/footer.jsx
+++ components/footer.jsx
@@ -25,11 +25,15 @@ const navigation = [
 
 export default function Footer() {
   return (
-    <footer className="bg-slate-50 dark:bg-gray-700">
-      <div className="px-4 py-12 mx-auto max-h-24 max-w-7xl sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
+    <footer className="bg-gray-50 dark:bg-gray-700">
+      <div className="px-4 py-8 mx-auto max-h-10 max-w-7xl sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
         <div className="flex justify-center space-x-6 md:order-2">
           {navigation.map((item) => (
-            <a key={item.name} href={item.href} className="text-gray-400 hover:text-gray-500">
+            <a
+              key={item.name}
+              href={item.href}
+              className="text-gray-400 hover:text-gray-500 dark:text-gray-100 dark:hover:text-gray-500"
+            >
               <span className="sr-only">{item.name}</span>
               <item.icon className="w-6 h-6" aria-hidden="true" />
             </a>
--- components/header/header.jsx
+++ components/header/header.jsx
@@ -58,7 +58,7 @@ const Header = () => {
               </div>
               <div className="flex items-center -mr-2 sm:hidden">
                 {/* Mobile menu button */}
-                <Disclosure.Button className="inline-flex items-center justify-center p-2 text-gray-400 bg-white rounded-md hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                <Disclosure.Button className="inline-flex items-center justify-center p-2 text-gray-800 rounded-md dark:text-gray-100">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
                     <XIcon className="block w-6 h-6" aria-hidden="true" />
@@ -80,7 +80,7 @@ const Header = () => {
                     className={classnames(
                       item.href === router.pathname
                         ? 'bg-indigo-50 dark:bg-gray-500 border-sky-500 text-sky-700 dark:text-gray-50'
-                        : 'border-transparent text-gray-600 dark:text-gray-300 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800',
+                        : 'border-transparent text-gray-600 dark:text-gray-300 hover:bg-gray-300 dark:hover:bg-gray-600 hover:border-gray-300 hover:text-gray-800',
                       'block pl-3 pr-4 py-2 border-l-4 text-base font-medium'
                     )}
                     aria-current={item.href === router.pathname ? 'page' : undefined}
