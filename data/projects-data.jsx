ba51a4ad98e758e6b2da86b40affa9f735b0c8f0
RUI <>
2022-02-28 05:04:13 +0900

    update projects

--- data/projects-data.jsx
+++ data/projects-data.jsx
@@ -1,58 +1,28 @@
 const projectsData = {
   en: [
     {
-      title: "A Search Engine",
-      description: `What if you could look up any information in the world? Webpages, images, videos
-      and more. Google has many features to help you find exactly what you're looking
-      for.`,
-      imgSrc: "/static/images/google.png",
-      href: "https://www.google.com",
-    },
-    {
-      title: "The Time Machine",
-      description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-      to the desired date and press "Go". No more worrying about lost keys or
-      forgotten handphones with this simple yet affordable solution.`,
-      imgSrc: "/static/images/time-machine.jpg",
-      href: "/blog/the-time-machine",
-    },
+      title: 'What birds',
+      description: `A project using AI to identify bird species online.`,
+      imgSrc: '/static/images/project-images/what-birds-logo.png',
+      href: 'https://www.whatbirds.com/'
+    }
   ],
   jp: [
     {
-      title: "A Search Engine",
-      description: `What if you could look up any information in the world? Webpages, images, videos
-      and more. Google has many features to help you find exactly what you're looking
-      for.`,
-      imgSrc: "/static/images/google.png",
-      href: "https://www.google.com",
-    },
-    {
-      title: "The Time Machine",
-      description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-      to the desired date and press "Go". No more worrying about lost keys or
-      forgotten handphones with this simple yet affordable solution.`,
-      imgSrc: "/static/images/time-machine.jpg",
-      href: "/blog/the-time-machine",
-    },
+      title: 'What birds',
+      description: `AIでオンラインで鳥の種類を識別するプロジェクト。`,
+      imgSrc: '/static/images/project-images/what-birds-logo.png',
+      href: 'https://www.whatbirds.com/'
+    }
   ],
-  fr: [
+  zh: [
     {
-      title: "Un moteur de recherche",
-      description: `Et si vous pouviez rechercher n'importe quelle information dans le monde ? Pages web, images, vidéos
-      et bien d'autres choses encore. Google propose de nombreuses fonctionnalités pour vous aider à trouver exactement ce que vous recherchez.
-      pour.`,
-      imgSrc: "/static/images/google.png",
-      href: "https://www.google.com",
-    },
-    {
-      title: "La machine à remonter le temps",
-      description: `Imaginez que vous puissiez voyager dans le passé ou dans le futur. Il suffit de tourner le bouton
-      sur la date souhaitée et d'appuyer sur "Go". Plus besoin de se soucier des clés perdues ou des téléphones portables oubliés avec cette solution simple et abordable.
-      ou de téléphones portables oubliés grâce à cette solution simple et abordable.`,
-      imgSrc: "/static/images/time-machine.jpg",
-      href: "/blog/the-time-machine",
-    },
-  ],
+      title: 'What birds',
+      description: `一个使用AI在线识别鸟类的项目。`,
+      imgSrc: '/static/images/project-images/what-birds-logo.png',
+      href: 'https://www.whatbirds.com/'
+    }
+  ]
 };
 
 export default projectsData;
